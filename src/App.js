709ff5cd3443043a0b09901b5399e5461180473f
Lukman10a <77152819+Lukman10a@users.noreply.github.com>
2022-03-15 17:12:00 +0100

    import Button component to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import * as React from "react";
+import Button from "./components/button";
+
 // import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="text-center font-semibold">
       <h1>Salafi AD Website</h1>
+      <div>
+        <Button text="Search" sym="" />
+      </div>
     </div>
   );
 }
