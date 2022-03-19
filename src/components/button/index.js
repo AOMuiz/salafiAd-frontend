86ae371018e4c9464478bd58e00f4fafd90347f8
Lukman10a <77152819+Lukman10a@users.noreply.github.com>
2022-03-19 22:13:49 +0100

    add some styles to the button componet

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -17,15 +17,17 @@ const Button = ({ text, sym, backgroundcolor = colors.blue }) => {
 export default Button;
 
 const Cont = styled.div`
-  color: ${colors.blue};
+  color: ${colors.white};
   display: inline-flex;
   padding: 1rem 4rem;
-  background-color: ${(props) => props.color};
+  /* background-color: ${(props) => props.color}; */
+  background-color: ${colors.blue};
   border-radius: 2px;
   cursor: pointer;
   &:hover {
     background-color: ${colors.white};
-    color: ${(props) => props.color};
+    /* color: ${(props) => props.color}; */
+    color: ${colors.blue};
     transition: 0.25s all ease-in-out;
     box-shadow: 3px 3px 7px rgba(25, 47, 18, 0.2);
   }
