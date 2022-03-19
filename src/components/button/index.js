e2a40eddef1828bf0e3d3b620e00a31d88d0f801
Lukman10a <77152819+Lukman10a@users.noreply.github.com>
2022-03-19 21:56:01 +0100

    made soome changes

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -2,9 +2,12 @@ import React from "react";
 import styled from "styled-components";
 import { colors } from "../../constants/fonts/colors";
 
-const Button = ({ text, sym }) => {
+const Button = ({ text, sym, backgroundcolor = colors.blue }) => {
   return (
-    <Cont className="flex justify-center align-center p-2 gap-2">
+    <Cont
+      className="flex justify-center align-center m-8 gap-2 "
+      color={colors}
+    >
       <div>{sym}</div>
       <div>{text}</div>
     </Cont>
@@ -14,5 +17,16 @@ const Button = ({ text, sym }) => {
 export default Button;
 
 const Cont = styled.div`
-  background-color: ${colors.blue};
+  color: ${colors.blue};
+  display: inline-flex;
+  padding: 1rem 4rem;
+  background-color: ${(props) => props.color};
+  border-radius: 2px;
+  cursor: pointer;
+  &:hover {
+    background-color: ${colors.white};
+    color: ${(props) => props.color};
+    transition: 0.25s all ease-in-out;
+    box-shadow: 3px 3px 7px rgba(25, 47, 18, 0.2);
+  }
 `;
