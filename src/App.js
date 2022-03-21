b23131dffb70c176a6c0917bd89d991324c0a2bf
Lukman10a <77152819+Lukman10a@users.noreply.github.com>
2022-03-21 21:39:21 +0100

    made some chanes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import Button from "./components/button";
+import HowItWorks from "./components/cards/worksCards";
 
 // import { Routes, Route, Link } from "react-router-dom";
 
@@ -9,6 +10,13 @@ function App() {
       <h1>Salafi AD Website</h1>
       <div>
         <Button text="Search" sym="yyy" />
+      </div>
+      <div>
+        <HowItWorks
+          title="Create Account"
+          subtitle=" Vestibulum Ante Ipsum Primis In Faucibus Orci Luctus Et Ultrices
+              Posuere Cubilia Curae. Donec Non Lorem Erat. Sed Vitae Vene."
+        />
       </div>
     </div>
   );
